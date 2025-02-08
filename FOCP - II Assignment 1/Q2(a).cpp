a69ccaadc1686ccd0bfb7b7dade6a734c1b6f36d
Raghav <24csu258@ncuindia.edu>
2025-02-08 18:35:43 +0530

    Create Q2(a).cpp

---
+++ FOCP - II Assignment 1/Q2(a).cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n,i;
+
+// Accept an integer array from the user
+
+    cout<<"Enter number of elements you want to input:"<<endl;
+    cin>>n;
+    int arr[n];
+
+    for(i=0;i<n;i++) {
+        cout<<"enter elements:"<<i+1<<"="<<" ";
+        cin>>arr[i];
+    }
+    cout<<"elements are:"<<endl;
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<endl;
+    }
+
+// Reverse the array and display it
+
+    cout<<"reverse elements are:"<< endl;
+    for(i=n-1;i>=0;i--) {
+        cout<<arr[i]<<endl;
+    }
+}
