3b1cbc3cef45ad1e7cafdbe0323b8a9a64416135
Raghav <24csu258@ncuindia.edu>
2025-02-08 16:46:42 +0530

    Create Q3(a).cpp

---
+++ FOCP - II Assignment 1/Q3(a).cpp
@@ -0,0 +1,31 @@
+// C++ program that accepts a string from the user and checks whether a string is a palindrome or not
+
+#include <iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+
+main() {
+
+    char string[20];
+    int i,length,flag=0;
+    cout<<"Enter any string";
+    cin>>string;
+    length=strlen(string);
+
+    for(i=0;i<length;i++){
+
+        if(string[i] != string[length-i-1]){
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag){
+        cout<<string<<"The input string is not a palindrome";
+    }
+
+    else
+    cout<<string<<"The input string is a palindrome";
+    getch();
+}
